af0b4829c9e6ee5395a67dac0f1e51481878cec1
zohaib <apricopt@gmail.com>
2021-04-07 18:51:00 +0000

    switched to sandbox mode

--- routes/restrecurring.js
+++ routes/restrecurring.js
@@ -14,22 +14,22 @@ if (process.env.NODE_ENV === "development") {
 //@step1 : Check whether you are working on testing env or live env
 
 // for testing
-// paypal.configure({
-// mode: "sandbox", //sandbox or live
-// client_id:
-// "ARMxqU6CNz_slWFJdYrr8mzQTxpVAEmDvfxNzPPFjOlgFf1ryZVeTJGQ599NrPGkCr15CRrmed3gCdBg",
-// client_secret:
-// "EK6WzJV2U73nKiqFRQG5EyaXH1phMEbVX056HTwn2o8pzO_H3BnqaToY4Q5koREyvyrKqZn8eXxogdHO",
-// });
+paypal.configure({
+mode: "sandbox", //sandbox or live
+client_id:
+"ARMxqU6CNz_slWFJdYrr8mzQTxpVAEmDvfxNzPPFjOlgFf1ryZVeTJGQ599NrPGkCr15CRrmed3gCdBg",
+client_secret:
+"EK6WzJV2U73nKiqFRQG5EyaXH1phMEbVX056HTwn2o8pzO_H3BnqaToY4Q5koREyvyrKqZn8eXxogdHO",
+});
 
 // for live mode
-paypal.configure({
-mode: "live", //sandbox or live
-client_id:
-"ATl7xXG5ctLoIbwxRmfXZDp4a_lFxxZp6Gk1pcjVab9hPvLUBxpZVA5FTuMesJFoAWcj9c33mbq4c8ha",
-client_secret:
-"EBZBcm2q0V9vL9raDHUidD12Kii8H-49v-ahdTokS2XFhzvIsq4Cogx5gP2Y35_VIjHNxC6zBSFtP8-t",
-});
+// paypal.configure({
+// mode: "live", //sandbox or live
+// client_id:
+// "ATl7xXG5ctLoIbwxRmfXZDp4a_lFxxZp6Gk1pcjVab9hPvLUBxpZVA5FTuMesJFoAWcj9c33mbq4c8ha",
+// client_secret:
+// "EBZBcm2q0V9vL9raDHUidD12Kii8H-49v-ahdTokS2XFhzvIsq4Cogx5gP2Y35_VIjHNxC6zBSFtP8-t",
+// });
 
 
 var isoDate = new Date();
@@ -185,7 +185,7 @@ paypal.billingPlan.create(billingPlanAttributes, function (error, billingPlan) {
         const member = new Member(dataToPush)
             member.save().then(result => {
             console.log("db mai saved member is this " , result)
-        }).then((err) => {
+        }).catch((err) => {
             console.log(err)
         });
                                 // See billing_agreements/execute.js to see example for executing agreement 
